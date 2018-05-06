b17d97c9d58caf1b95890554460eeb4442e9606f
HongkyBayu <ranggaaabayu@icloud.com>
2018-05-06 14:28:49 +0700

    Change blumarine logo

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ include "admin/connectiondb.php";
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top"><img style="height:50px;" class="img-responsive" src="img/blue-marine1.gif"  alt="Blue Marine"></a>
+        <a class="navbar-brand js-scroll-trigger" href="#page-top"><img style="height:80px;" class="img-responsive" src="img/LOGO.png"  alt="Blue Marine"></a>
         <button style="background-color:dodgerblue;" class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fa fa-bars"></i>
